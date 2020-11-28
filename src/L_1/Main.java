55f1438add1b6e3712ddd1f93e9435dd31be6491
Dmitry Kokhna <dmitry.kokhna@gmail.com>
2020-11-28 23:29:56 +0300

    корректировка

--- src/L_1/Main.java
+++ src/L_1/Main.java
@@ -1,12 +1,30 @@
 package L_1;
 
 
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        PC firstPC = new PC(3500, "Dell");
+public class Main{
+        public static void main(String[] args) {
+         PC firstPC = new PC();
+        Scanner sc=new Scanner(System.in);
+        int z=sc.nextInt();
+        firstPC.setCpu(z);
         firstPC.printPCttx();
-        Notebook firstNote=new Notebook(2900,"Lenovo",15);
+        Notebook firstNote = new Notebook(3500, "Lenovo", 15);
         firstNote.printPCttx();
+ //PC.battery =6200;
+        int a = firstPC.getCpu();
+        int b = firstNote.getCpu();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("Проц  ПК " + firstPC.getCpu());
+
+     //   PC.battery=1000;
+      //  System.out.println(PC.battery);
+      //  System.out.println(PC.BoostBatt());
+
     }
 }
+
+
+
